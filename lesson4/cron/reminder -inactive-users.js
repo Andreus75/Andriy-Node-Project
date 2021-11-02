b46f3a287eb6644828565b5eb97b6c162fd69dc6
Andreus75 <t_andriy@ukr.net>
2021-11-02 09:48:10 +0200

    node.js homework lesson11.+ mail sender.

--- lesson4/cron/reminder -inactive-users.js
+++ lesson4/cron/reminder -inactive-users.js
@@ -11,8 +11,7 @@ module.exports = async () => {
     const previousMonth = dayJs.utc().subtract(1, 'month');
 
     const users = await User
-        .find({updatedAt: { $lt: previousMonth }})
-        .populate('_id');
+        .find({updatedAt: { $lt: previousMonth }});
 
     await Promise.allSettled(users.map(user => emailService.sendMail(user.email, REMINDER, {userName: user.name})));
 };
